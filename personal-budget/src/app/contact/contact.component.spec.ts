06a21fb2c57809ff4883690a6da3c8808ea50a93
YashAnetz <55511290+YashAnetz@users.noreply.github.com>
2023-09-24 23:54:56 -0400

    Second commit

--- personal-budget/src/app/contact/contact.component.spec.ts
+++ personal-budget/src/app/contact/contact.component.spec.ts
@@ -1,17 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ContactComponent } from './contact.component';
 
-import { MenuComponent } from './menu.component';
-
-describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
+describe('ContactComponent', () => {
+  let component: ContactComponent;
+  let fixture: ComponentFixture<ContactComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [MenuComponent]
+      declarations: [ContactComponent]
     });
-    fixture = TestBed.createComponent(MenuComponent);
+    fixture = TestBed.createComponent(ContactComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
