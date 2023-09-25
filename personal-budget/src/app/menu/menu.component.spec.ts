72338d3a0c47981af24b372bcd4cffb44c1024b1
YashAnetz <55511290+YashAnetz@users.noreply.github.com>
2023-09-25 02:23:33 -0400

    Final Commit

--- personal-budget/src/app/menu/menu.component.spec.ts
+++ personal-budget/src/app/menu/menu.component.spec.ts
@@ -1,16 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-
 import { MenuComponent } from './menu.component';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [MenuComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MenuComponent ]
+    })
+    .compileComponents();
+
     fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
